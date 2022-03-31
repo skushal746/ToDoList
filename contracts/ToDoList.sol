b00fe9669a3a41a1392b51df8afc71a2ac6bcc3c
Kushal Sharma <skushal746@gmail.com>
2022-03-31 19:27:13 -0400

    Added front end files

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -2,6 +2,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract ToDoList {
     uint public taskCount = 0;
-    
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out this first blockchain application implementation");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 
 }
